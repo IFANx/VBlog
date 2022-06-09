0e226928006dd40685910202535bec33285e8ee1
pivot <18865290907@163.com>
2022-06-09 16:28:23 +0800

    initial project and improve some basic structure

---
+++ VblogService/src/main/java/com/example/vblogservice/VblogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.vblogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VblogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VblogServiceApplication.class, args);
+    }
+
+}
---
+++ VblogService/src/test/java/com/example/vblogservice/VblogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vblogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VblogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
