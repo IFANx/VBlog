849265500d614640864bb9cfceae2f6904c9a41b
ChenJunzhe <49234358+wildpantss@users.noreply.github.com>
2022-06-17 15:02:38 +0800

    init frontend module, and test importing bootstrap.

---
+++ vblogfront/README.md
@@ -0,0 +1,31 @@
+# vblogfront
+
+Front-end project for vblog, using vue3 + bootstrap5
+
+## Project setup
+
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+
+```
+npm run build
+```
+
+### Lints and fixes files
+
+```
+npm run lint
+```
+
+### Customize configuration
+
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ vblogfront/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div class="container">
+    <form class="form-login">
+      <h1 class="h3 mb-3 font-weight-normal">Bootstrap Component Import Test</h1>
+      <label for="inputEmail" class="sr-only">Email Address</label>
+      <input type="email" id="inputEmail" class="form-control mb-2" placeholder="Email Address" required autofocus>
+      <label for="inputPassword" class="sr-only">Password</label>
+      <input type="password" class="form-control mb-2" id="inputPassword" placeholder="Password" required>
+      <div class="checkbox mb-3">
+        <label>
+          <input type="checkbox" value="remember-me"/>Remember me
+        </label>
+      </div>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+    </form>
+  </div>
+
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
