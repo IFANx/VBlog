3265c425624e13656ffc0cf9efedab8a22220619
pivot <18865290907@163.com>
2022-06-20 13:42:08 +0800

    websocket create

---
+++ VblogService/src/main/java/com/example/vblogservice/service/NotificationWebsocketServer.java
@@ -0,0 +1,64 @@
+package com.example.vblogservice.service;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.stereotype.Component;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+@ServerEndpoint("/ws/notification/{account}")
+public class NotificationWebsocketServer {
+    private static Map<String, NotificationWebsocketServer> online = new ConcurrentHashMap<String, NotificationWebsocketServer>();
+    private static int onlineCount = 0;
+    private Session session;
+    private String account;
+
+    @OnOpen
+    public void onOpen(Session session, @PathParam("account") String account) {
+        this.session = session;
+        this.account = account;
+
+        addOnlineCount();
+        online.put(account, this);
+        System.out.println(getOnlineCount());
+    }
+
+    @OnClose
+    public void onClose(){
+        online.remove(account);
+        subOnlineCount();
+    }
+
+    @OnMessage
+    public void OnMessage(String message) throws IOException {
+        JSONObject jsonTo = JSONObject.parseObject(message);
+        String mes = (String) jsonTo.get("message");
+
+        for (NotificationWebsocketServer item : online.values()) {
+            if(item.account.equals(jsonTo.get("to"))) {
+                item.session.getAsyncRemote().sendText(mes);
+            }
+        }
+    }
+
+    public static synchronized void addOnlineCount() {
+        NotificationWebsocketServer.onlineCount++;
+    }
+
+    public static synchronized void subOnlineCount() {
+        NotificationWebsocketServer.onlineCount--;
+    }
+
+    public static synchronized int getOnlineCount() {
+        return onlineCount;
+    }
+
+}
