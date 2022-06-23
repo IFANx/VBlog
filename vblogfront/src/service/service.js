0137175a5f6a45d9b2258b6a378d3550beafe05d
pivot <18865290907@163.com>
2022-06-23 14:43:41 +0800

    config axios header

---
+++ vblogfront/src/service/service.js
@@ -0,0 +1,41 @@
+import axios from 'axios'
+
+const instance = axios.create({
+    timeout: 300000,
+})
+
+instance.interceptors.request.use(
+    (config) => {
+        config.headers['BEARER'] = this.$store.state.token
+        config.headers['account'] = this.$store.state.account
+        return config
+    },
+    (error) => {
+        return Promise.reject(error)
+    }
+)
+
+instance.interceptors.response.use(
+    (response) => {
+        let tokenTmp = response.headers['BEARER']
+        if (tokenTmp !== this.$store.state.token) {
+            this.commit('setToken', tokenTmp)
+        }
+
+        return response
+    },
+    (error) => {
+        return Promise.reject(error)
+    }
+)
+
+const api = {
+    get(url, data) {
+        return instance.get(url, data)
+    },
+    post(url, data) {
+        return instance.post(url, data)
+    }
+}
+
+export {api}
--- vblogfront/src/store/index.js
+++ vblogfront/src/store/index.js
@@ -2,10 +2,22 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    onlineState: false,
+    account: null,
+    token: null
   },
   getters: {
   },
   mutations: {
+    setOnlineState(onlineState, result) {
+      this.state.onlineState = result
+    },
+    setAccount(account, result) {
+      this.state.account = result
+    },
+    setToken(token, result) {
+      this.state.token = result
+    }
   },
   actions: {
   },
