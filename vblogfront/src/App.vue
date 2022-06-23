bca72cdca607e1e80d508edef7fa13f1a4aacc42
许康2017141461270 <2804271734@qq.com>
2022-06-23 22:59:16 +0800

    HomeView、CommentView页面

--- vblogfront/src/App.vue
+++ vblogfront/src/App.vue
@@ -31,9 +31,9 @@
   <div class="b-example-divider"></div>
 
   <nav>
-    <router-link to="/">Home</router-link>
-    |
-    <router-link to="/about">About</router-link>
+<!--    <router-link to="/">Home</router-link>-->
+<!--    |-->
+<!--    <router-link to="/about">About</router-link>-->
   </nav>
 
 
