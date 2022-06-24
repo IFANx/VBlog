8011b3976668e0bdb2d70abcc7794d4c95716716
ChenJunzhe <49234358+wildpantss@users.noreply.github.com>
2022-06-24 21:43:08 +0800

    frontend: add time format parse util and add icons on articleView

--- vblogfront/src/service/utils.js
+++ vblogfront/src/service/utils.js
@@ -1,7 +1,28 @@
 import {api} from "../service/service.js"
 
+const parseTimeToDate = (datetime) => {
+    let time = new Date(datetime)
+    let year = time.getFullYear()
+    let month = time.getMonth()
+    let day = time.getDay()
+    return year + '-' + month + '-' + day
+}
+
+const parseTimeToDateTime = (datetime) => {
+    let time = new Date(datetime)
+    let year = time.getFullYear()
+    let month = time.getMonth()
+    let day = time.getDay()
+    let hour = time.getHours()
+    let min = time.getMinutes()
+    let sec = time.getSeconds()
+    return year + '-' + month + '-' + day + ' ' + hour + ':' + min + ':' + sec
+}
 
 /** Export all functions. */
-const utils = {}
+const utils = {
+    parseTimeToDate,
+    parseTimeToDateTime
+}
 
 export default utils
