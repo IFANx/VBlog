857dd857d3666d014f4fb12512f135162f93abfc
ChenJunzhe <49234358+wildpantss@users.noreply.github.com>
2022-06-24 21:13:35 +0800

    frontend: add comment pagination.

--- vblogfront/src/views/article/ArticleView.vue
+++ vblogfront/src/views/article/ArticleView.vue
@@ -23,24 +23,48 @@
         </article>
 
         <!--    Comment Area    -->
-        <div class="table table-borderless">
-          <table class="table align-middle">
-            <thead class="toast-header">
-            Comment
-            </thead>
+        <container>
+          <div class="table table-borderless">
+            <table class="table align-middle">
+              <thead class="toast-header">
+              Comment
+              </thead>
 
-            <tbody>
-            <tr v-for="item in comment" v-bind:key="item.user">
-              <td>commenter id = {{ item.commenterId }}</td>
-              <td>comment time = {{ item.commentTime }}</td>
-              <td>comment content = {{ item.content }}</td>
-            </tr>
-            </tbody>
-          </table>
-        </div>
+              <tbody>
+              <tr v-for="item in comment" v-bind:key="item.user">
+                <td>commenter id = {{ item.commenterId }}</td>
+                <td>comment time = {{ item.commentTime }}</td>
+                <td>comment content = {{ item.content }}</td>
+              </tr>
+              </tbody>
+            </table>
+          </div>
 
+          <!--     Pagination NavBar     -->
+          <nav aria-label="Page Navigation">
+            <ul class="pagination">
+              <li class="page-item" @click="toTargetPage(this.pagination.currentPage-1)"><a class="page-link">Prev
+                Page</a></li>
+              <template v-for="i in this.pagination.totalPages">
+                <!--   use active item if it's current page   -->
+                <li class="page-item active" v-if="pagination.currentPage==i">
+                  <a class="page-link" @click="toTargetPage(i)">{{ i }}</a>
+                </li>
+                <!--   normal item if not   -->
+                <li class="page-item" v-if="pagination.currentPage!=i">
+                  <a class="page-link" @click="toTargetPage(i)">{{ i }}</a>
+                </li>
+              </template>
+              <li class="page-item" @click="toTargetPage(this.pagination.currentPage+1)"><a class="page-link">Next
+                Page</a></li>
+            </ul>
+          </nav>
+        </container>
+
+        <!--    New Comment    -->
         <div class="position-absolute buttom-0 start-50 translate-middle-x">
-          <label for="description" class="form-label" style="font-size: 20px;font-weight:bolder">评论框</label>
+          <label for="description" class="form-label" style="font-size: 20px;font-weight:bolder">Add your comment
+            here</label>
           <textarea style="width:1000px" v-model="description" class="form-control" id="description"
                     rows="2"></textarea>
           <button class="btn btn-outline-primary" href="#">提交</button>
@@ -56,7 +80,6 @@
 </template>
 
 <script>
-import comment from "@/service/comment";
 
 export default {
   name: 'ArticleView',
@@ -65,9 +88,29 @@ export default {
       article: {}, // init when view loaded.
       comment: {}, // init when view loaded.
       publisher: {}, // init when view loaded.
+      pagination: {
+        currentPage: 1,
+        pageSize: 10,
+        total: 0,
+        totalPages: 0
+      }
     }
   },
   methods: {
+    toTargetPage(pageIndex) {
+      // low/upper bound check and fix.
+      if (pageIndex > this.pagination.totalPages) pageIndex = this.pagination.totalPages
+      if (pageIndex < 1) pageIndex = 1
+      // to request paginated data.
+      this.$api.comment.getCommentByArticlePaged(this.article.userId, pageIndex, this.pagination.pageSize).then(
+          (response) => {
+            if (response.data.code === '0000') {
+              this.comment = response.data.data.list
+              this.pagination.currentPage = pageIndex
+            }
+          }
+      )
+    },
     fetchArticle(id) {
       this.$api.article.getArticleById(id).then(
           (response) => {
@@ -103,6 +146,8 @@ export default {
             if (response.data.code === '0000') {
               // set comments if request success.
               this.comment = response.data.data.list
+              this.pagination.total = response.data.data.total
+              this.pagination.totalPages = response.data.data.pages
             } else {
               console.log(response.data.message)
             }
@@ -115,7 +160,7 @@ export default {
   mounted() {
     // execute this method when current view loaded.
     this.fetchArticle(this.$route.query.id) // request article model from backend.
-    this.fetchComments(this.$route.query.id, 1, 5) // request 1st page of comments from backend.
+    this.fetchComments(this.$route.query.id, 1, 10) // request 1st page of comments from backend.
   }
 }
 </script>
