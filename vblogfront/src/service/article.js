c74bbc8733a057c329bf43beb945e8df56a468ad
ChenJunzhe <49234358+wildpantss@users.noreply.github.com>
2022-06-24 15:56:16 +0800

    frontend: adapt and fix article axios api. (adapt with backend param form)

--- vblogfront/src/service/article.js
+++ vblogfront/src/service/article.js
@@ -3,7 +3,7 @@ import {api} from "../service/service.js"
 /** APIs for article resources HERE. */
 
 const getArticleById = (id) => {
-    return api.get('article/', {id: id})
+    return api.get('article', {id: id})
 }
 
 const countArticle = () => {
@@ -11,55 +11,55 @@ const countArticle = () => {
 }
 
 const getArticlesPaged = (startPage, pageSize) => {
-    return api.get('articles/paged/', {startPage: startPage, pageSize: pageSize})
+    return api.get('articles/paged', {startPage: startPage, pageSize: pageSize})
 }
 
 const countArticleByTitle = (title) => {
-    return api.get('article/counts/title/', {title: title})
+    return api.get('article/counts/title', {title: title})
 }
 
 const countArticleByFuzzyTitle = (title) => {
-    return api.get('article/counts/title/fuzzy/', {title: title})
+    return api.get('article/counts/title/fuzzy', {title: title})
 }
 
 const getArticlesByTitlePaged = (title, startPage, pageSize) => {
-    return api.get('articles/title/paged/', {title: title, startPage: startPage, pageSize: pageSize})
+    return api.get('articles/title/paged', {title: title, startPage: startPage, pageSize: pageSize})
 }
 
 const getArticleByFuzzyTitlePaged = (title, startPage, pageSize) => {
-    return api.get('articles/title/fuzzy/paged/', {title: title, startPage: startPage, pageSize: pageSize})
+    return api.get('articles/title/fuzzy/paged', {title: title, startPage: startPage, pageSize: pageSize})
 }
 
 const countArticleByTag = (tag) => {
-    return api.get('article/counts/tag/', {tag: tag})
+    return api.get('article/counts/tag', {tag: tag})
 }
 
 const getArticlesByTagPaged = (tag, startPage, pageSize) => {
-    return api.get('articles/tag/paged/', {tag: tag, startPage: startPage, pageSize: pageSize})
+    return api.get('articles/tag/paged', {tag: tag, startPage: startPage, pageSize: pageSize})
 }
 
 const countArticleByUserId = (userId) => {
-    return api.get('article/counts/user_id/', {userId: userId})
+    return api.get('article/counts/user_id', {userId: userId})
 }
 
 const getArticlesByUserIdPaged = (userId, startPage, pageSize) => {
-    return api.get('articles/user_id/paged/', {userId: userId, startPage: startPage, pageSize: pageSize})
+    return api.get('articles/user_id/paged', {userId: userId, startPage: startPage, pageSize: pageSize})
 }
 
 const countArticleByPublishTime = (publishTime) => {
-    return api.get('article/counts/publish_time/', {publishTime: publishTime})
+    return api.get('article/counts/publish_time', {publishTime: publishTime})
 }
 
 const countArticleByPublishTimeRange = (startTime, endTime) => {
-    return api.get('article/counts/publish_time/', {startTime: startTime, endTime: endTime})
+    return api.get('article/counts/publish_time', {startTime: startTime, endTime: endTime})
 }
 
 const getArticlesByPublishTimePaged = (publishTime, startPage, pageSize) => {
-    return api.get('articles/publish_time/paged/', {publishTime: publishTime, startPage: startPage, pageSize: pageSize})
+    return api.get('articles/publish_time/paged', {publishTime: publishTime, startPage: startPage, pageSize: pageSize})
 }
 
 const getArticlesByPublishTimeRangePaged = (startTime, endTime, startPage, pageSize) => {
-    return api.get('articles/publish_time/paged/', {
+    return api.get('articles/publish_time/paged', {
         startTime: startTime,
         endTime: endTime,
         startPage: startPage,
@@ -72,7 +72,7 @@ const insertArticle = (articleObj) => {
 }
 
 const deleteArticle = (id) => {
-    return api.delete('article/', id)
+    return api.delete('article', id)
 }
 
 const updateArticle = (articleObj) => {
